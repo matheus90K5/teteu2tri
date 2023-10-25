ad1b140582a5db2bc2c484a6b569ccd044b0d5d8
matheus90K5 <114937584+matheus90K5@users.noreply.github.com>
2023-10-25 09:08:59 -0300

    Add files via upload

---
+++ tabuada.js
@@ -0,0 +1,15 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector("pre")
+
+//ouvinte
+frm.addEventListener("submit", (e) =>{
+    e.preventDefault()
+
+    const numero = Number(frm.inNumero.value)
+    let resposta = ""   //variavel do tipo Strimg, para concatenar a resposta 
+//criar um laço de repetição
+    for(let i = 1; i <=10; i++){
+        resposta =resposta + numero + "x " + i + "=" + (numero * i) +"\n" 
+    }
+    resp.innerText = resposta 
+})
